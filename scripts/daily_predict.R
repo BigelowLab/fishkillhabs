74818ac2013fed591d66ff95d13cf8166a8491e5
Johnathan Evanilla <jevanilla@bigelow.org>
2026-07-28 17:48:43 -0400

    updating daily prediction workflow

--- scripts/daily_predict.R
+++ scripts/daily_predict.R
@@ -4,51 +4,13 @@
 
 source("setup.R")
 
-species = "Alexandrium catenella"
 model_v = "v3"
-
-cfg = read_configuration(scientificname = species,
-                         version = model_v, 
-                         path = data_path("models"))
 
 # load global covariates
 
 daily_covar = read_covariates_daily()
 
-# load pre-trained model
-
-file = gsub(" ", "-", sprintf("%s-%s-model_fits", species, model_v))
-
-model_fit = read_model_fit(filename = file) |>
-  filter(wflow_id %in% c("default_rf"))
-model_fit
-
-
-# predict - one species
-
-model = workflows::extract_fit_engine(model_fit$.workflow[[1]])
-
-mtype = model_fit_spec(model_fit)
-
-p = predict(daily_covar, model, type = get_response_type(mtype))[1]
-
-colors = c("magma",
-           "inferno",
-           "plasma",
-           "viridis",
-           "cividis",
-           "rocket",
-           "mako",
-           "turbo")[1]
-
-risk_map = ggplot2::ggplot() +
-  stars::geom_stars(data = p) + 
-  ggplot2::scale_fill_viridis_c(option = colors[1], 
-                                limits = c(0,1), 
-                                na.value = "grey50")
-risk_map
-
-species <- c(#"Margalefidinium polykrikoides",
+species <- c("Margalefidinium polykrikoides",
              "Heterosigma akashiwo",
              "Noctiluca scintillans", 
              "Karenia mikimotoi",
@@ -58,7 +20,7 @@ species <- c(#"Margalefidinium polykrikoides",
              "Pseudochattonella verruculosa",
              "Prymnesium polylepis",
              "Chrysochromulina leadbeateri")
-model_v = "v3"
+
 risk_maps = lapply(species,
                    function(s) {
                      cfg = read_configuration(scientificname = s,
@@ -77,30 +39,62 @@ risk_maps = lapply(species,
                      p = predict(daily_covar, model, type = get_response_type(mtype))[1]
                      names(p) = "predicted_probability"
                      
-                     outpath_stars = file.path("predictions", sprintf("%s-riskmap-%s.tif", 
-                                                                      gsub(" ", "_", s, fixed = TRUE),
-                                                                      Sys.Date()))
+                     outpath_stars = file.path("predictions", 
+                                               gsub(" ", "_", tolower(s), fixed = TRUE), 
+                                               sprintf("%s-riskmap.tif", 
+                                                       gsub(" ", "_", s, fixed = TRUE)))
                      
-                     write_stars(p, outpath_stars)
-                     
-                     outpath_table = file.path("predictions", sprintf("%s-riskmap-%s.csv.gz", 
-                                                                      gsub(" ", "_", s, fixed = TRUE),
-                                                                      Sys.Date()))
-                     as.data.frame(p) |> 
-                       filter(!is.na(predicted_probability)) |>
-                       write_csv(outpath_table)
+                     write_stars(p, outpath_stars,driver="COG")
                    })
 
 # format for google cloud
 
-outfile = file.path("predictions", sprintf("%s-riskmap-%s.tif", 
-                                            gsub(" ", "_", species[1], fixed = TRUE),
-                                            Sys.Date()))
-
-write_stars(p, outfile)
-
 # write to cloud storage
 
+## one species example
 
 
+#species = "Margalefidinium polykrikoides"
+#model_v = "v3"
+#
+#cfg = read_configuration(scientificname = species,
+#                         version = model_v, 
+#                         path = data_path("models"))
+## load pre-trained model
+#
+#file = gsub(" ", "-", sprintf("%s-%s-model_fits", species, model_v))
+#
+#model_fit = read_model_fit(filename = file) |>
+#  filter(wflow_id %in% c("default_rf"))
+#model_fit
+#
+#
+## predict - one species
+#
+#model = workflows::extract_fit_engine(model_fit$.workflow[[1]])
+#
+#mtype = model_fit_spec(model_fit)
+#
+#p = predict(daily_covar, model, type = get_response_type(mtype))[1]
+#
+#colors = c("magma",
+#           "inferno",
+#           "plasma",
+#           "viridis",
+#           "cividis",
+#           "rocket",
+#           "mako",
+#           "turbo")[1]
+#
+#risk_map = ggplot2::ggplot() +
+#  stars::geom_stars(data = p) + 
+#  ggplot2::scale_fill_viridis_c(option = colors[1], 
+#                                limits = c(0,1), 
+#                                na.value = "grey50")
+#risk_map
+#outfile = file.path("predictions", sprintf("%s-riskmap-%s.tif", 
+#                                           gsub(" ", "_", species[1], fixed = TRUE),
+#                                           Sys.Date()))
+#
+#write_stars(p, outfile)
 
